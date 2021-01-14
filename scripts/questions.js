31d464f49e7f2149ad5bac48f27e0095851c2bf3
AlIbay8 <43456952+AlIbay8@users.noreply.github.com>
2021-01-13 16:07:22 -0800

    Jeopardy MVP completed all requirements and added a few extensions

--- scripts/questions.js
+++ scripts/questions.js
@@ -1,139 +1,265 @@
-let questions_by_category = [{
-        category: "Robots",
-        100: {
-            question_text: "SPOILER ALERT: HIS DYING WORDS IN THE ANIMATED TRANSFORMERS MOVIE ARE \"TIL ALL ARE ONE\"",
-            answer_text: "Who is Optimus Prime?",
-        },
-        200: {
-            question_text: "THIS ROBOT BOUNTY HUNTER WAS HIRED TO FIND THE MILLENNIUM FALCON IN THE EMPIRE STRIKES BACK",
-            answer_text: "Who is IG-88 or 4-LOM?"
-        },
-        300: {
-            question_text: "ROBOTS B.O.B. AND V.I.N.C.E.N.T. PROVIDED SOME COMIC RELIEF IN THIS DARK 1979 DISNEY SCI-FI FILM",
-            answer_text: "What is The Black Hole?"
-        },
-        400: {
-            question_text: "CHRIS JERICHO HOSTED THIS AUTOMATON FIGHTING COMPETITION ON SYFY IN EARLY 2013",
-            answer_text: "What is Robot Combat League?"
-        },
-        500: {
-            question_text: "HE'S THE ROBOT WHO GETS ELECTROCUTED AND COMES TO LIFE IN 1986'S SHORT CIRCUIT",
-            answer_text: "Who is Johnny 5/Number 5?"
-        }
+var questions_list = [
+  {
+    category: "Robots",
+    200: {
+      question_text:
+        'SPOILER ALERT: HIS DYING WORDS IN THE ANIMATED TRANSFORMERS MOVIE ARE "TIL ALL ARE ONE"',
+      answer_text: "Who is Optimus Prime?"
     },
-    {
-        category: "Nicknames",
-        100: {
-            question_text: "MINECRAFT CREATOR MARKUS PERSSON IS BETTER KNOWN BY THIS MONOSYLLABIC NICKNAME",
-            answer_text: "What is Notch?",
-        },
-        200: {
-            question_text: "IT'S THE ALTER EGO OF PENNY ARCADE'S MIKE KRAHULIK - AND THE NAME OF MIKE'S SON",
-            answer_text: "What is Gabe?"
-        },
-        300: {
-            question_text: "ESPORTSCASTER AND MACHINIMA PERSONALITY RYAN WYATT IS BEST KNOWN BY THIS SHORT NICKNAME",
-            answer_text: "What is Fwiz?"
-        },
-        400: {
-            question_text: "IN ADDITION TO \"CLIFFYB\", CLIFF BLESZINSKI IS SOMETIMES KNOWN BY THIS LARGE NICKNAME, BRO",
-            answer_text: "What is Dude Huge?"
-        },
-        500: {
-            question_text: "LARRY HRYB BORROWED HIS GAMERTAG, \"MAJOR NELSON\", FROM THIS 1960S SITCOM",
-            answer_text: "What is I Dream of Jeannie?"
-        }
+    400: {
+      question_text:
+        "THIS ROBOT BOUNTY HUNTER WAS HIRED TO FIND THE MILLENNIUM FALCON IN THE EMPIRE STRIKES BACK",
+      answer_text: "Who is IG-88 or 4-LOM?"
     },
-    {
-        category: "Famous first words",
-        100: {
-            question_text: "\"BITE MY SHINY METAL ASS\"",
-            answer_text: "Who is Bender?",
-        },
-        200: {
-            question_text: "\"WHY, YOU SLIMY, DOUBLE-CROSSING, NO-GOOD SWINDLER. YOU GOT A LOT OF GUTS COMING HERE, AFTER WHAT YOU PULLED\"",
-            answer_text: "Who is Lando Calrissian?"
-        },
-        300: {
-            question_text: "\"LISTEN TO MY STORY. THIS MAY BE OUR LAST CHANCE.\"",
-            answer_text: "Who is Tidus?"
-        },
-        400: {
-            question_text: "\"FARMBOY, POLISH MY HORSE'S SADDLE. I WANT TO SEE MY FACE SHINING IN IT BY MORNING\"",
-            answer_text: "Who is Princess Buttercup?"
-        },
-        500: {
-            question_text: "\"THIS IS IT. THIS IS WHERE FORRESTAL CASHED IN HIS CHIPS\"",
-            answer_text: "Who is Indiana Jones?"
-        }
+    600: {
+      question_text:
+        "ROBOTS B.O.B. AND V.I.N.C.E.N.T. PROVIDED SOME COMIC RELIEF IN THIS DARK 1979 DISNEY SCI-FI FILM",
+      answer_text: "What is The Black Hole?"
     },
-    {
-        category: "Going 'dark'",
-        100: {
-            question_text: "THIS UNFORGIVING ACTION/RPG FROM FROM SOFTWARE TAKES PLACE IN THE KINGDOM OF LORDRAN",
-            answer_text: "What is Dark Souls?",
-        },
-        200: {
-            question_text: "TIM BURTON AND JOHNNY DEPP RESURRECTED THIS LATE-60S VAMPIRE SOAP OPERA AS A COMEDY FILM IN 2012",
-            answer_text: "What is Dark Shadows?"
-        },
-        300: {
-            question_text: "THIS CLASSIC FRANK MILLER GRAPHIC NOVEL FEATURES A BITTER, AGING BATMAN AND A FEMALE ROBIN",
-            answer_text: "What is The Dark Knight Returns?"
-        },
-        400: {
-            question_text: "GELFLINGS KIRA AND JEN ESCAPE THE SKEKSIS IN THIS 1982 JIM HENSON CULT FANTASY FAVORITE",
-            answer_text: "What is The Dark Crystal?"
-        },
-        500: {
-            question_text: "THE SPIDER-MAN STAGE MUSICAL, NOW PLAYING ON BROADWAY, FEATURES THIS UNUSUAL SUBTITLE",
-            answer_text: "What is Turn Off the Dark?"
-        }
+    800: {
+      question_text:
+        "CHRIS JERICHO HOSTED THIS AUTOMATON FIGHTING COMPETITION ON SYFY IN EARLY 2013",
+      answer_text: "What is Robot Combat League?"
     },
-    {
-        category: "Fictional companies",
-        100: {
-            question_text: "YOU CAN BLAME THIS FICTIONAL COMPANY FOR RESIDENT EVIL'S T-VIRUS",
-            answer_text: "What is Umbrella?",
-        },
-        200: {
-            question_text: "\"MORE HUMAN THAN HUMAN\" IS THE MOTTO OF THIS REPLICANT MANUFACTURER FROM BLADE RUNNER",
-            answer_text: "What is Tyrell Corporation?"
-        },
-        300: {
-            question_text: "MILES DYSON CREATED NEURAL-NET PROCESSORS FOR THIS COMPANY, WHICH ULTIMATELY BUILDS SKYNET",
-            answer_text: "What is Cyberdyne Systems?"
-        },
-        400: {
-            question_text: "THEY OWN ROBOCOP, ED-209, AND THE ENTIRE DETROIT POLICE FORCE",
-            answer_text: "What is Omni Consumer Products/OCP?"
-        },
-        500: {
-            question_text: "IN TRON, KEVIN FLYNN HACKS HIS WAY INTO THE NETWORK OF THIS FICTIONAL GAME PUBLISHER",
-            answer_text: "What is Encom?"
-        }
-    },
-    {
-        category: "Painfully obvious",
-        100: {
-            question_text: "SHE'S THE PRINCESS IN THE CLASSIC RPG THE LEGEND OF ZELDA",
-            answer_text: "Who is Zelda?",
-        },
-        200: {
-            question_text: "BUNGIE'S WORKING TITLE FOR MARATHON WAS MARATHON, BUT ONI'S WORKING TITLE WAS THIS",
-            answer_text: "What is Oni?"
-        },
-        300: {
-            question_text: "THIS TWO-BUTTON FIGHTING GAME STARS TWO GUYS NAMED KICK AND DIVE",
-            answer_text: "What is Divekick?"
-        },
-        400: {
-            question_text: "BRODERBUND'S CASTLE WOLFENSTEIN WAS AN INSPIRATION FOR THIS 1992 FIRST-PERSON SHOOTER FROM ID SOFTWARE",
-            answer_text: "What is Wolfenstein 3D?"
-        },
-        500: {
-            question_text: "THIS VIDEO GAME CHARACTER IS BLUE, COLLECTS RINGS, AND GOES FAST",
-            answer_text: "What is Sonic?"
-        }
+    1000: {
+      question_text:
+        "HE'S THE ROBOT WHO GETS ELECTROCUTED AND COMES TO LIFE IN 1986'S SHORT CIRCUIT",
+      answer_text: "Who is Johnny 5/Number 5?"
     }
+  },
+  {
+    category: "Nicknames",
+    200: {
+      question_text:
+        "MINECRAFT CREATOR MARKUS PERSSON IS BETTER KNOWN BY THIS MONOSYLLABIC NICKNAME",
+      answer_text: "What is Notch?"
+    },
+    400: {
+      question_text:
+        "IT'S THE ALTER EGO OF PENNY ARCADE'S MIKE KRAHULIK - AND THE NAME OF MIKE'S SON",
+      answer_text: "What is Gabe?"
+    },
+    600: {
+      question_text:
+        "ESPORTSCASTER AND MACHINIMA PERSONALITY RYAN WYATT IS BEST KNOWN BY THIS SHORT NICKNAME",
+      answer_text: "What is Fwiz?"
+    },
+    800: {
+      question_text:
+        'IN ADDITION TO "CLIFFYB", CLIFF BLESZINSKI IS SOMETIMES KNOWN BY THIS LARGE NICKNAME, BRO',
+      answer_text: "What is Dude Huge?"
+    },
+    1000: {
+      question_text:
+        'LARRY HRYB BORROWED HIS GAMERTAG, "MAJOR NELSON", FROM THIS 1960S SITCOM',
+      answer_text: "What is I Dream of Jeannie?"
+    }
+  },
+  {
+    category: "Famous first words",
+    200: {
+      question_text: '"BITE MY SHINY METAL ASS"',
+      answer_text: "Who is Bender?"
+    },
+    400: {
+      question_text:
+        '"WHY, YOU SLIMY, DOUBLE-CROSSING, NO-GOOD SWINDLER. YOU GOT A LOT OF GUTS COMING HERE, AFTER WHAT YOU PULLED"',
+      answer_text: "Who is Lando Calrissian?"
+    },
+    600: {
+      question_text: '"LISTEN TO MY STORY. THIS MAY BE OUR LAST CHANCE."',
+      answer_text: "Who is Tidus?"
+    },
+    800: {
+      question_text:
+        '"FARMBOY, POLISH MY HORSE\'S SADDLE. I WANT TO SEE MY FACE SHINING IN IT BY MORNING"',
+      answer_text: "Who is Princess Buttercup?"
+    },
+    1000: {
+      question_text:
+        '"THIS IS IT. THIS IS WHERE FORRESTAL CASHED IN HIS CHIPS"',
+      answer_text: "Who is Indiana Jones?"
+    }
+  },
+  {
+    category: "Going 'dark'",
+    200: {
+      question_text:
+        "THIS UNFORGIVING ACTION/RPG FROM FROM SOFTWARE TAKES PLACE IN THE KINGDOM OF LORDRAN",
+      answer_text: "What is Dark Souls?"
+    },
+    400: {
+      question_text:
+        "TIM BURTON AND JOHNNY DEPP RESURRECTED THIS LATE-60S VAMPIRE SOAP OPERA AS A COMEDY FILM IN 2012",
+      answer_text: "What is Dark Shadows?"
+    },
+    600: {
+      question_text:
+        "THIS CLASSIC FRANK MILLER GRAPHIC NOVEL FEATURES A BITTER, AGING BATMAN AND A FEMALE ROBIN",
+      answer_text: "What is The Dark Knight Returns?"
+    },
+    800: {
+      question_text:
+        "GELFLINGS KIRA AND JEN ESCAPE THE SKEKSIS IN THIS 1982 JIM HENSON CULT FANTASY FAVORITE",
+      answer_text: "What is The Dark Crystal?"
+    },
+    1000: {
+      question_text:
+        "THE SPIDER-MAN STAGE MUSICAL, NOW PLAYING ON BROADWAY, FEATURES THIS UNUSUAL SUBTITLE",
+      answer_text: "What is Turn Off the Dark?"
+    }
+  },
+  {
+    category: "Fictional companies",
+    200: {
+      question_text:
+        "YOU CAN BLAME THIS FICTIONAL COMPANY FOR RESIDENT EVIL'S T-VIRUS",
+      answer_text: "What is Umbrella?"
+    },
+    400: {
+      question_text:
+        '"MORE HUMAN THAN HUMAN" IS THE MOTTO OF THIS REPLICANT MANUFACTURER FROM BLADE RUNNER',
+      answer_text: "What is Tyrell Corporation?"
+    },
+    600: {
+      question_text:
+        "MILES DYSON CREATED NEURAL-NET PROCESSORS FOR THIS COMPANY, WHICH ULTIMATELY BUILDS SKYNET",
+      answer_text: "What is Cyberdyne Systems?"
+    },
+    800: {
+      question_text:
+        "THEY OWN ROBOCOP, ED-209, AND THE ENTIRE DETROIT POLICE FORCE",
+      answer_text: "What is Omni Consumer Products/OCP?"
+    },
+    1000: {
+      question_text:
+        "IN TRON, KEVIN FLYNN HACKS HIS WAY INTO THE NETWORK OF THIS FICTIONAL GAME PUBLISHER",
+      answer_text: "What is Encom?"
+    }
+  },
+  {
+    category: "Painfully obvious",
+    200: {
+      question_text:
+        "SHE'S THE PRINCESS IN THE CLASSIC RPG THE LEGEND OF ZELDA",
+      answer_text: "Who is Zelda?"
+    },
+    400: {
+      question_text:
+        "BUNGIE'S WORKING TITLE FOR MARATHON WAS MARATHON, BUT ONI'S WORKING TITLE WAS THIS",
+      answer_text: "What is Oni?"
+    },
+    600: {
+      question_text:
+        "THIS TWO-BUTTON FIGHTING GAME STARS TWO GUYS NAMED KICK AND DIVE",
+      answer_text: "What is Divekick?"
+    },
+    800: {
+      question_text:
+        "BRODERBUND'S CASTLE WOLFENSTEIN WAS AN INSPIRATION FOR THIS 1992 FIRST-PERSON SHOOTER FROM ID SOFTWARE",
+      answer_text: "What is Wolfenstein 3D?"
+    },
+    1000: {
+      question_text:
+        "THIS VIDEO GAME CHARACTER IS BLUE, COLLECTS RINGS, AND GOES FAST",
+      answer_text: "What is Sonic?"
+    }
+  },
+  {
+    category: "Virtual reality",
+    200: {
+      question_text:
+        "With its unique time dilation mechanic, this game is the most innovative shooter I've played in years!",
+      answer_text: "What is SUPERHOT?"
+    },
+    400: {
+      question_text:
+        "This game lets you slash blocks in choreographed directions to the beat of the music with light sabers.",
+      answer_text: "What is Beat Saber?"
+    },
+    600: {
+      question_text:
+        "The flagship AAA game designed for VR from Valve. This game puts you up against headcrabs, antlions and the Combine.",
+      answer_text: "What is Half-Life: Alyx?"
+    },
+    800: {
+      question_text:
+        "Valve's premium VR headset which features individual finger tracking on its controllers.",
+      answer_text: "What is the Valve Index?"
+    },
+    1000: {
+      question_text: "The founder of Oculus, before Facebook was inolved.",
+      answer_text: "Who is Palmer Luckey?"
+    }
+  },
+  {
+    category: "Half-Life",
+    200: {
+      question_text:
+        "This parasite attaches to the face of its host and controls them even after death.",
+      answer_text: "What is a headcrab?"
+    },
+    400: {
+      question_text:
+        "In Half-Life 2 and the episodes, they are the oppressive government who built the Citadel in City 17.",
+      answer_text: "Who are the Combine?"
+    },
+    600: {
+      question_text:
+        "The main protagonist of Half-Life 1, Half-Life 2, and the Episodes of Half-Life 2.",
+      answer_text: "Who is Gordon Freeman?"
+    },
+    800: {
+      question_text:
+        "The most mysterious character in the Half-Life series. He hires Gordon Freeman at the end of Half-Life 1 and Alyx Vance at the end of Half-Life: Alyx.",
+      answer_text: "Who is the G-Man?"
+    },
+    1000: {
+      question_text: "He owes Gordon Freeman a beer.",
+      answer_text: "Who is Barney Calhoun?"
+    }
+  },
+  {
+    category: "Portal",
+    200: {
+      question_text: "The company that developed the Handheld Portal Gun.",
+      answer_text: "Who is Aperture Science?"
+    },
+    400: {
+      question_text:
+        "This person preserved Chell in the Aperture Science Extended Relaxation Center and kept Aperture Science facilities running until Chell's awakening.",
+      answer_text: "Who is Doug Rattmann?"
+    },
+    600: {
+      question_text:
+        "This was the first mechanic being developed for Portal 2. Also referring to the value of the Aperture opening of a camera, it was scrapped for not being relevant to portals.",
+      answer_text: "What is F-Stop."
+    },
+    800: {
+      question_text: "",
+      answer_text: ""
+    },
+    1000: {
+      question_text: "",
+      answer_text: ""
+    }
+  }
 ];
+
+var uniqueIndices = [];
+while (uniqueIndices.length < 6) {
+  var r = Math.floor(Math.random() * questions_list.length);
+  if (uniqueIndices.indexOf(r) === -1) uniqueIndices.push(r);
+}
+console.log(uniqueIndices);
+
+let questions_by_category = [
+  questions_list[uniqueIndices[0]],
+  questions_list[uniqueIndices[1]],
+  questions_list[uniqueIndices[2]],
+  questions_list[uniqueIndices[3]],
+  questions_list[uniqueIndices[4]],
+  questions_list[uniqueIndices[5]]
+];
+
+console.log(questions_by_category[0].category);
